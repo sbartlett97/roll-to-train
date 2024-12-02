295cdae6c9c38e86e0b570ae788ca9ef8f65628b
sam <sam.bartlett858@gmail.com>
2024-12-02 20:38:15 +0000

    shuffle datasets and run for whole train set

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ def main(intelligence=15, dc=12, dataset=None):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     dataset = load_dataset("zeroshot/twitter-financial-news-sentiment", split="train")
     val_dataset = load_dataset("zeroshot/twitter-financial-news-sentiment", split="validation")
-    dataloader = DataLoader(dataset, batch_size=4)
-    val_dataloader = DataLoader(val_dataset, batch_size=4)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=True)
     optimizer = AdamW(model.parameters(), lr=5e-5)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
 
     trainer = DnDTrainer(model, tokenizer, optimizer, scheduler, intelligence, dc)
-    trainer.train(dataloader, val_dataloader, steps=1000, eval_steps=100)
+    trainer.train(dataloader, val_dataloader, steps=len(dataloader), eval_steps=100)
 
 
 
